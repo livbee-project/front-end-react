71d9eef4874a79bd5aa7b38beda85737ddd80e36
hwicoding <dlgnl117@gmail.com>
2025-11-04 22:48:22 +0900

    251104 > front-end > react > 홈 > 상단 탭 고정 해제

--- livbee-project/src/app/Router.tsx
+++ livbee-project/src/app/Router.tsx
@@ -7,38 +7,20 @@ import TopNavLayout from '../presentation/layouts/TopNavLayout';
 const AppRouter = () => (
   <BrowserRouter>
     {/*
-      (수정) .app-container를 <Home /> 내부가 아닌 라우터의 최상위로 이동시켰습니다.
-      Flutter의 max-width 1200px를 모든 페이지에 일관되게 적용합니다.
+      (수정) .app-container에서 style={{ height: '100vh' }} 를 제거합니다.
+      이제 이 div는 페이지 컨텐츠의 길이에 따라 자연스럽게 늘어납니다.
     */}
-    <div className="app-container" style={{ height: '100vh' }}>
+    <div className="app-container">
       <Routes>
-        {/*
-          --- (수정) 중첩 라우트(Shell Route) 적용 ---
-          
-          1. 부모 <Route>가 TopNavLayout을 렌더링합니다.
-             이 부모 <Route>는 path가 없습니다.
-        */}
         <Route element={<TopNavLayout />}>
-          {/*
-            2. 자식 <Route>들
-            이제 이 경로들은 <TopNavLayout>의 <Outlet /> 내부에 렌더링됩니다.
-            Flutter 라우터의 자식 경로들을 참고했습니다.
-          */}
+          {/* ... (자식 라우트들은 변경 없음) ... */}
           <Route path="/" element={<Home />} />
-
-          {/* (추가) 탭바에 있는 나머지 페이지들의 라우트 (임시) */}
           <Route path="/clips" element={<div>클립 페이지</div>} />
           <Route path="/live" element={<div>쇼핑라이브 페이지</div>} />
           <Route path="/news" element={<div>뉴스 페이지</div>} />
           <Route path="/event" element={<div>이벤트 페이지</div>} />
           <Route path="/service" element={<div>서비스 페이지</div>} />
         </Route>
-
-        {/*
-          (참고) 만약 /login 처럼 탭바가 *없는* 페이지를 만든다면,
-          이 <Route> 그룹 밖에 별도로 선언하면 됩니다.
-        */}
-        {/* <Route path="/login" element={<div>로그인 페이지</div>} /> */}
       </Routes>
     </div>
   </BrowserRouter>
